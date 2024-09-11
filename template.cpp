b5b1de1a5642b4fa371da86d1dc4cef9e522dfd3
GZhonghui <mail@gzher.com>
2024-09-11 21:58:31 +0900

    更新模版

--- template.cpp
+++ template.cpp
@@ -12,6 +12,13 @@
 using namespace std;
 
 typedef int64_t ll;
+typedef double f;
+
+const ll mod = 1e9+7;
+const ll maxn = 2e5 + 8;
+const ll inf = INT64_MAX;
+const f pi = acos(-1.0);
+const f eps = 1e-6;
 
 int main()
 {
