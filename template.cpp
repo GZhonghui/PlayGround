4d7b2543bef41ec5f11c9fb7750c388f0f604d2a
GZhonghui <mail@gzher.com>
2024-09-12 01:51:20 +0900

    [misc] 方向常量

--- template.cpp
+++ template.cpp
@@ -15,6 +15,9 @@ using namespace std; // 只在算法比赛中这样写
 typedef int64_t ll;
 typedef double f;
 
+const ll dx[4] = {0,0,1,-1};
+const ll dy[4] = {1,-1,0,0};
+
 const ll mod = 1e9+7;
 const ll maxn = 2e5 + 8;
 const ll inf = INT64_MAX;
