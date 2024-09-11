40a3942626656b7afed97a0e6bb13e649eb24eef
GZhonghui <mail@gzher.com>
2024-09-11 21:18:58 +0900

    整个模版

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdint>
+#include <climits>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+typedef int64_t ll;
+
+int main()
+{
+
+    return 0;
+}
