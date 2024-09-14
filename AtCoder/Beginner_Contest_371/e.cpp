a83d37bdd9d3cf240b07d1554b26e2d2f9cbbfbe
GZhonghui <mail@gzher.com>
2024-09-15 02:20:15 +0900

    [补题] 区间计数

--- AtCoder/Beginner_Contest_371/e.cpp
+++ AtCoder/Beginner_Contest_371/e.cpp
@@ -25,16 +25,85 @@ const ll inf = INT64_MAX;
 const f pi = acos(-1.0);
 const f eps = 1e-6;
 
-ll n,a[maxn];
+ll n,a[maxn], ans = 0;
+unordered_map<ll,vector<ll> > c;
 
 int main()
 {
+    // freopen("in.txt","r",stdin);
+
     cin >> n;
+    ll total = ((1 + n) * n) >> 1;
+
     for(ll i=1;i<=n;i+=1)
     {
         cin >> a[i];
+        c[a[i]].push_back(i);
+        /*
+        if(!c.count(a[i])) c[a[i]] = 0;
+
+        ll l = 1, r = c.size();
+        ll delta = 0; //(((l + r) * (r - l + 1)) >> 1);
+
+        ans += delta; // + c[a[i]];
+        cout << i << ": delta >= " << delta << endl;
+
+        ll delta_right = 0;
+        for(ll j=1;j<=i;j+=1)
+        {
+            unordered_set<ll> cc;
+            for(ll k=j;k<=i;k+=1)
+            {
+                cc.insert(a[k]);
+            }
+            delta_right += cc.size();
+        }
+        cout << i << ": right delta = " << delta_right << endl;
+        c[a[i]] += 1;
+        */
     }
 
-    
+    // 转换思路
+    // 总区间个数是固定的，是total
+    // 对于每一个数字，出现在了这total个区间中的哪几个呢？
+    // 出现在了几个区间中，这个数字的贡献就是几
+    // 本体关键还是还是考虑，每个数字是怎样对最终结果作出贡献的
+    for(auto i=c.begin();i!=c.end();i++)
+    {
+        ll left = 0, this_ans = total;
+        vector<ll>& pos = i->second;
+        if(!pos.size()) continue;
+
+        pos.push_back(n+1);
+        for(ll j=0;j<pos.size();j+=1)
+        {
+            ll right = pos[j];
+            ll len = right - left - 1;
+            this_ans -= ((1 + len) * len) >> 1;
+            left = right;
+        }
+        ans += this_ans;
+    }
+
+    /*
+    ll ans1 = 0;
+    for(ll i=1;i<=n;i+=1)
+    {
+        for(ll j=i;j<=n;j+=1)
+        {
+            unordered_set<ll> cc;
+            for(ll k=i;k<=j;k+=1)
+            {
+                cc.insert(a[k]);
+            }
+            ans1 += cc.size();
+        }
+    }
+
+    cout << ans1 << endl;
+    */
+
+    cout << ans << endl;
+
     return 0;
 }
