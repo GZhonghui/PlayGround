ddd9c68848aca9e633a55ff8d19cd7853040ed76
GZhonghui <mail@gzher.com>
2024-10-27 00:17:38 +0900

    [补题] 置换 经典习题 值得复习

--- AtCoder/Beginner_Contest_377/e.cpp
+++ AtCoder/Beginner_Contest_377/e.cpp
@@ -7,9 +7,16 @@
 // Q1：为什么是2^k次？
 // A：简单来说，数学归纳法
 // 其实在推导的过程中，自己的公式中也出现了很多次2^k这个数字，但是没有想到是用在这里
+// TODO：详细解释
 
 // Q2：有没有可能是先一段环外的路径，再进入环，这样的话就不好计算了
 // A：不可能，每个节点入度为1，出度为1，可能想象，就是若干个环，没有环外的部分
+
+// Q3：本题和ABC367E有什么区别？
+// A：本题的P会改变，所以不能累加（或者说不方便累加，从这里理解为什么是2^k）
+// ABC367E的P不会改变，是P作用在其他的数组上，所以可以累加
+
+// 另外，把图画出来，顺着路径走一走，可以帮助理解为什么是2^k
 
 #include <algorithm>
 #include <iostream>
@@ -51,10 +58,23 @@ struct edge {
     ll to,dis;
 };
 
+uint64_t qpow(uint64_t base, uint64_t times, uint64_t mod)
+{
+    if(times == 0) return 1;
+    if(times == 1) return base;
+
+    uint64_t ans = qpow(base, times>>1, mod) % mod;
+    
+    ans = ans * ans % mod;
+    if(times % 2) ans = ans * base % mod;
+
+    return ans;
+}
+
 vector<edge> edges;
 vector<ll> G[maxn]; // id of target OR id of edge
 
-ll n,k,pp[maxl][maxn];
+ll n,k,p[maxn],ans[maxn];
 
 int main()
 {
@@ -63,17 +83,36 @@ int main()
 #endif
 
     cin >> n >> k;
-    rep(i, 1, n) cin >> pp[0][i];
+    rep(i, 1, n) cin >> p[i];
 
-    rep(i, 1, maxl - 1)
-    {
-        rep(j, 1, n) pp[i][j] = pp[i-1][pp[i-1][j]];
+    memset(ans,-1,sizeof(ans));
+
+    rep(i, 1, n) {
+        if(ans[i] != -1) continue;
+
+        vector<ll> loop;
+        unordered_set<ll> flag;
+
+        ll now = i;
+        while(!flag.count(now))
+        {
+            flag.insert(now);
+            loop.push_back(now);
+
+            now = p[now];
+        }
+
+        ll loop_size = loop.size();
+
+        ll rk = qpow(2, k, loop_size);
+
+        rep(j, 0, loop_size - 1)
+        {
+            ans[loop[j]] = loop[(j + rk) % loop_size];
+        }
     }
 
-    if(k < maxl)
-    {
-        rep(i, 1, n) cout << pp[k][i] << " " << endl;
-    }
+    rep(i, 1, n) cout << ans[i] << " "; cout << endl;
 
     return 0;
 }
