3e1a1f9c6fa0f41c7313e5cc63976197ae563568
GZhonghui <mail@gzher.com>
2025-01-12 00:25:25 +0900

    [补题] Simultaneous Kagamimochi 2

---
+++ AtCoder/Beginner_Contest_388/g.cpp
@@ -0,0 +1,215 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>  // for setprecision
+#include <cstdint>
+#include <climits>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#include <atcoder/all>
+
+#define mk make_pair
+#define pb push_back
+#define rep(i, s, t) for (ll i = (s); i <= (t); i++)
+#define rre(i, s, t) for (ll i = (t); i >= (s); i--)
+
+using namespace std; // only in algorithm contests
+
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef long double f;
+
+const int dx[4] = {-1,0,1,0};
+const int dy[4] = {0,1,0,-1};
+
+const ll maxn = 2e5 + 8;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9 + 8;
+const f pi = acos(-1.0);
+const f eps = 1e-6;
+
+template<class T> struct point {
+    T x,y;
+
+    T dis2(const point &r) {
+        T dx = x - r.x, dy = y - r.y;
+        return dx * dx + dy * dy;
+    }
+};
+
+typedef point<ll> pl;
+typedef point<f> pf;
+
+struct edge {
+    ll to,dis;
+};
+
+vector<edge> edges;
+vector<ll> g[maxn]; // id of target OR id of edge
+
+ll n,a[maxn],r_target[maxn];
+
+ll op(ll a, ll b) {
+    return max(a, b);
+}
+
+ll e() {
+    return 0;
+}
+
+struct SGTree
+{
+    int N,Record[maxn];
+    int mmax[maxn*4],mmin[maxn*4],ssum[maxn*4];
+    int lazy[maxn*4],Left,Right,Value;
+    void getRecord() // 应该是获取初始值
+    {
+        scanf("%d",&N);
+        for(int i=1;i<=N;i++) scanf("%d",&Record[i]);
+    }
+    void build(int node,int l,int r)
+    {
+        lazy[node]=0;
+        if(l==r)
+        {
+            mmax[node]=mmin[node]=ssum[node]=Record[l];
+            return;
+        }
+        int mid=(l+r)>>1;
+        build(node<<1,l,mid);
+        build((node<<1)+1,mid+1,r);
+        ssum[node]=ssum[node<<1]+ssum[(node<<1)+1];
+        mmax[node]=max(mmax[node<<1],mmax[(node<<1)+1]);
+        mmin[node]=min(mmin[node<<1],mmin[(node<<1)+1]);
+    }
+    void pushDown(int node,int l,int r)
+    {
+        int mid=(l+r)>>1;
+        lazy[node<<1]=lazy[(node<<1)+1]=lazy[node];
+        ssum[node<<1]=lazy[node]*(mid-l+1);
+        ssum[(node<<1)+1]=lazy[node]*(r-mid);
+        mmax[node<<1]=mmax[(node<<1)+1]=lazy[node];
+        mmin[node<<1]=mmin[(node<<1)+1]=lazy[node];
+        lazy[node]=0;
+    }
+    void Update(int node,int l,int r)
+    {
+        if(Left<=l&&r<=Right)
+        {
+            lazy[node]=Value;
+            ssum[node]=Value*(r-l+1);
+            mmax[node]=mmin[node]=Value;
+            return;
+        }
+        int mid=(l+r)>>1;
+        if(lazy[node]) pushDown(node,l,r);
+        if(Left<=mid) Update(node<<1,l,mid);
+        if(Right>mid) Update((node<<1)+1,mid+1,r);
+        ssum[node]=ssum[node<<1]+ssum[(node<<1)+1];
+        mmax[node]=max(mmax[node<<1],mmax[(node<<1)+1]);
+        mmin[node]=min(mmin[node<<1],mmin[(node<<1)+1]);
+    }
+    int AskMax(int node,int l,int r)
+    {
+        if(Left<=l&&r<=Right) return mmax[node];
+        if(lazy[node]) pushDown(node,l,r);
+        int mid=(l+r)>>1;
+        bool exl=(Left<=mid),exr=(Right>mid);
+        if(exl&&exr) return max(AskMax(node<<1,l,mid),AskMax((node<<1)+1,mid+1,r));
+        if(exl) return AskMax(node<<1,l,mid); return AskMax((node<<1)+1,mid+1,r);
+    }
+    int AskMin(int node,int l,int r)
+    {
+        if(Left<=l&&r<=Right) return mmin[node];
+        if(lazy[node]) pushDown(node,l,r);
+        int mid=(l+r)>>1;
+        bool exl=(Left<=mid),exr=(Right>mid);
+        if(exl&&exr) return min(AskMin(node<<1,l,mid),AskMin((node<<1)+1,mid+1,r));
+        if(exl) return AskMin(node<<1,l,mid); return AskMin((node<<1)+1,mid+1,r);
+    }
+    int AskSum(int node,int l,int r)
+    {
+        if(Left<=l&&r<=Right) return ssum[node];
+        if(lazy[node]) pushDown(node,l,r);
+        int mid=(l+r)>>1,ans=0;
+        if(Left<=mid) ans+=AskSum(node<<1,l,mid);
+        if(Right>mid) ans+=AskSum((node<<1)+1,mid+1,r);
+        return ans;
+    }
+}ac;
+
+atcoder::segtree<ll, op, e> tree(maxn);
+
+inline bool can_make(ll cnt, ll l, ll r) {
+    if(!cnt) return true;
+
+    ll begin = l, end = l + cnt - 1;
+    ac.Left = begin, ac.Right = end;
+    ll max_d = ac.AskMax(1,1,ac.N);
+
+    ll length = cnt + max(cnt, max_d);
+    ll right = l + length - 1;
+    return right <= r;
+}
+
+int main()
+{
+#ifdef ZH_DEBUG
+    freopen("in.txt", "r", stdin);
+#endif
+    cin >> n;
+    ac.N = n;
+
+    rep(i,1,n) {
+        cin >> a[i];
+        ac.Record[i] = a[i];
+    }
+    ac.build(1,1,ac.N);
+    
+    ll r_idx = 1;
+    rep(i,1,n) {
+        ll now = a[i];
+
+        while(r_idx <= n && a[r_idx] < now * 2) r_idx += 1;
+        r_target[i] = r_idx;
+        // tree.set(i, r_target[i] - i);
+        ac.Left = i, ac.Right = i, ac.Value = r_target[i] - i;
+        ac.Update(1,1,ac.N);
+    }
+
+    ll q;
+    cin >> q;
+    while(q--) {
+        ll l,r;
+        cin >> l >> r;
+
+        ll _l = 0, _r = (r - l + 1) >> 1;
+        while(_r - _l > 3) {
+            ll _m = (_l + _r) >> 1;
+            if(can_make(_m, l, r)) {
+                _l = _m;
+            } else {
+                _r = _m - 1;
+            }
+        }
+
+        for(ll _t = _r; _t >= _l; _t -= 1) {
+            if(can_make(_t, l, r)) {
+                cout << _t << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
