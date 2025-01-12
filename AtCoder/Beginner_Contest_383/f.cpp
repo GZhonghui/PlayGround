0bb8488f7701c29b2ead1deca6d7efdb3a2b986c
GZhonghui <mail@gzher.com>
2025-01-12 18:27:03 +0900

    [补题] DP 首先按照某个维度排序

--- AtCoder/Beginner_Contest_383/f.cpp
+++ AtCoder/Beginner_Contest_383/f.cpp
@@ -19,6 +19,7 @@
 #define mk make_pair
 #define pb push_back
 #define rep(i, s, t) for (ll i = (s); i <= (t); i++)
+#define rre(i, s, t) for (ll i = (t); i >= (s); i--)
 
 using namespace std; // only in algorithm contests
 
@@ -30,7 +31,8 @@ const int dx[4] = {-1,0,1,0};
 const int dy[4] = {0,1,0,-1};
 
 const ll mod = 1e9+7;
-const ll maxn = 2e5 + 8;
+const ll maxn = 500 + 8;
+const ll maxx = 50000 +8;
 const ll inf = INT64_MAX;
 const f pi = acos(-1.0);
 const f eps = 1e-6;
@@ -54,14 +56,53 @@ struct edge {
 vector<edge> edges;
 vector<ll> g[maxn]; // id of target OR id of edge
 
-ll n,k;
+struct item {
+    ll p,u,c;
+
+    bool operator<(const item &other) const {
+        return c < other.c;
+    }
+};
+
+ll n,x,k,dp[maxx],cache[maxx];
+vector<item> items;
 
 int main()
 {
 #ifdef ZH_DEBUG
     freopen("in.txt", "r", stdin);
 #endif
-
+    cin >> n >> x >> k;
+    rep(i,1,n) {
+        static ll p,u,c;
+        cin >> p >> u >> c;
+        items.pb((item){p,u,c});
+    }
+    sort(items.begin(), items.end());
+    
+    ll current_color = -1;
+    rep(i,1,x) cache[i] = dp[i] = -1; cache[0] = dp[0] = 0;
+    for(item &i : items) {
+        if(i.c != current_color) {
+            rep(j,0,x) {
+                cache[j] = dp[j] = max(dp[j], cache[j]);
+            }
+            current_color = i.c;
+        }
+        rre(j,0,x){
+            if(j >= i.p) {
+                if(dp[j - i.p] >= 0) {
+                    dp[j] = max(dp[j], dp[j - i.p] + i.u);
+                }
+                if(cache[j - i.p] >= 0) {
+                    dp[j] = max(dp[j], cache[j - i.p] + i.u + k);
+                }
+            }
+        }
+    }
+    ll ans = 0;
+    rep(i,0,x) ans = max(ans, max(dp[i], cache[i]));
+    cout << ans << endl;
 
     return 0;
 }
