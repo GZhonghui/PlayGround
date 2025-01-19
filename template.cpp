b35aea3011580ca07ae29649e11af0d61002b89f
GZhonghui <mail@gzher.com>
2025-01-20 01:50:30 +0900

    更新模板

--- template.cpp
+++ template.cpp
@@ -1,3 +1,5 @@
+// C++ >= 17
+
 #include <algorithm>
 #include <iostream>
 #include <iomanip>  // for setprecision
@@ -6,6 +8,7 @@
 #include <cstring>
 #include <cstdio>
 #include <vector>
+#include <random>
 #include <stack>
 #include <queue>
 #include <deque>
@@ -16,12 +19,15 @@
 #include <set>
 #include <unordered_set>
 
+#include <atcoder/all>
+
 #define mk make_pair
 #define pb push_back
 #define rep(i, s, t) for (ll i = (s); i <= (t); i++)
 #define rre(i, s, t) for (ll i = (t); i >= (s); i--)
 
 using namespace std; // only in algorithm contests
+using namespace atcoder;
 
 typedef int64_t ll;
 typedef uint64_t ull;
@@ -36,6 +42,81 @@ const ll inf = 1e9 + 8;
 const f pi = acos(-1.0);
 const f eps = 1e-6;
 
+// lazysegtree BEGIN
+
+struct S_both {
+    ll sum;
+    ll max;
+    ll len;
+};
+
+struct F_both {
+    ll add;
+    ll set;
+    bool is_set;
+};
+
+S_both op_both(S_both a, S_both b) {
+    return S_both{
+        a.sum + b.sum,
+        max(a.max, b.max),
+        a.len + b.len
+    };
+}
+
+S_both e_both() {
+    return S_both{0, (ll)-1e18, 0};
+}
+
+S_both mapping_both(F_both f, S_both s) {
+    if (f.is_set) {
+        return S_both{
+            f.set * s.len,
+            f.set,
+            s.len
+        };
+    } else {
+        return S_both{
+            s.sum + f.add * s.len,
+            s.max + f.add,
+            s.len
+        };
+    }
+}
+
+F_both composition_both(F_both new_op, F_both old_op) {
+    if (new_op.is_set) {
+        return new_op;
+    } else if (old_op.is_set) {
+        return F_both{new_op.add + old_op.add, old_op.set + new_op.add, true};
+    } else {
+        return F_both{new_op.add + old_op.add, 0, false};
+    }
+}
+
+F_both id_both() {
+    return F_both{0, 0, false};
+}
+
+inline void lazy_segtree_toolkit() {
+    vector<S_both> initial(8, {0, 0, 1}); // avaliable range: [0, 8)
+    lazy_segtree<S_both, op_both, e_both, F_both, mapping_both, composition_both, id_both> seg(initial);
+
+    // set data of pos x
+    seg.set(1, {5, 5, 1});
+    // get data of pos x
+    S_both res_single = seg.get(1);
+
+    // apply operation to range [l, r)
+    seg.apply(1, 5, F_both{3, 0, false}); // [1, 5)
+    // query data of range [l, r)
+    S_both res_range = seg.prod(2, 6); // [2, 6)
+}
+
+// lazysegtree END
+
+// geometry BEGIN
+
 template<class T> struct point {
     T x,y;
 
@@ -48,6 +129,10 @@ template<class T> struct point {
 typedef point<ll> pl;
 typedef point<f> pf;
 
+// geometry END
+
+// graph BEGIN
+
 struct edge {
     ll to,dis;
 };
@@ -55,7 +140,9 @@ struct edge {
 vector<edge> edges;
 vector<ll> g[maxn]; // id of target OR id of edge
 
-ll n,k;
+// graph END
+
+ll n;
 
 int main()
 {
