0cf12ebc30f4767b17311ae2954072f663fcf2f1
GZhonghui <mail@gzher.com>
2025-01-23 03:11:51 +0900

    [补题] ABC001

--- AtCoder/Beginner_Contest_001/a.cpp
+++ AtCoder/Beginner_Contest_001/a.cpp
@@ -31,9 +31,7 @@ using namespace atcoder;
 
 typedef int64_t ll;
 typedef uint64_t ull;
-typedef double f; // 使用 long double 会 WA 我真是想不通！！！折磨我一晚上
-// 更让人生气的是，评测结果 sample 是 WA 但是我本地都是 AC
-// 可能是评测机的编译器版本问题
+typedef long double f; // may cause WA...
 
 const int dx[4] = {-1,0,1,0};
 const int dy[4] = {0,1,0,-1};
@@ -151,84 +149,10 @@ int main()
 #ifdef ZH_DEBUG
     freopen("in.txt", "r", stdin);
 #endif
-    ll deg_i, dis;
-    cin >> deg_i >> dis;
 
-    f speed = dis / 60.0;
-    ll speed_i = speed * 10 + 0.5;
-
-    if(speed_i <= 2) {
-        cout << "C 0" << endl;
-        return 0;
-    }
-
-    ll speed_level = 0;
-
-    if (speed_i <= 15) {
-        speed_level = 1;
-    } else if (speed_i <= 33) {
-        speed_level = 2;
-    } else if (speed_i <= 54) {
-        speed_level = 3;
-    } else if (speed_i <= 79) {
-        speed_level = 4;
-    } else if (speed_i <= 107) {
-        speed_level = 5;
-    } else if (speed_i <= 138) {
-        speed_level = 6;
-    } else if (speed_i <= 171) {
-        speed_level = 7;
-    } else if (speed_i <= 207) {
-        speed_level = 8;
-    } else if (speed_i <= 244) {
-        speed_level = 9;
-    } else if (speed_i <= 284) {
-        speed_level = 10;
-    } else if (speed_i <= 326) {
-        speed_level = 11;
-    } else {
-        speed_level = 12;
-    }
-
-    f deg = deg_i * 0.1;
-
-    string dir;
-
-    if (11.25 < deg && deg < 33.75) {
-        dir = "NNE";
-    } else if (33.75 < deg && deg < 56.25) {
-        dir = "NE";
-    } else if (56.25 < deg && deg < 78.75) {
-        dir = "ENE";
-    } else if (78.75 < deg && deg < 101.25) {
-        dir = "E";
-    } else if (101.25 < deg && deg < 123.75) {
-        dir = "ESE";
-    } else if (123.75 < deg && deg < 146.25) {
-        dir = "SE";
-    } else if (146.25 < deg && deg < 168.75) {
-        dir = "SSE";
-    } else if (168.75 < deg && deg < 191.25) {
-        dir = "S";
-    } else if (191.25 < deg && deg < 213.75) {
-        dir = "SSW";
-    } else if (213.75 < deg && deg < 236.25) {
-        dir = "SW";
-    } else if (236.25 < deg && deg < 258.75) {
-        dir = "WSW";
-    } else if (258.75 < deg && deg < 281.25) {
-        dir = "W";
-    } else if (281.25 < deg && deg < 303.75) {
-        dir = "WNW";
-    } else if (303.75 < deg && deg < 326.25) {
-        dir = "NW";
-    } else if (326.25 < deg && deg < 348.75) {
-        dir = "NNW";
-    } else {
-        dir = "N";
-    }
-
-    cout << dir << " " << speed_level << endl;
+    ll a,b;
+    cin >> a >> b;
+    cout << a - b << endl;
 
     return 0;
 }
