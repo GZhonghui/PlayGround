463b1bfc26d2af1724efd1f4c6bfb9d552dbc01e
GZhonghui <mail@gzher.com>
2025-02-09 01:02:31 +0900

    [ACL] 并查集

---
+++ AtCoder/Library_Practice_Contest/A_Disjoint_Set_Union.cpp
@@ -0,0 +1,44 @@
+/*
+
+一些ACL共通的内容：
+区间都是左闭右开，下标都是0-indexed，C++17以上语言标准
+
+并查集
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <atcoder/all>
+
+namespace Disjoint_Set_Union {
+    using namespace atcoder;
+    using namespace std;
+
+    using ll = int64_t;
+
+    inline void solve() {
+        ll n, q;
+        cin >> n >> q;
+
+        dsu d(n); // [0, n)
+
+        while(q--) {
+            static ll t, u, v;
+            cin >> t >> u >> v;
+
+            if(t == 0) {
+                d.merge(u, v);
+            } else {
+                cout << (d.same(u, v) ? 1 : 0) << endl;
+            }
+        }
+    }
+}
+
+int main()
+{
+    Disjoint_Set_Union::solve();
+    return 0;
+}
