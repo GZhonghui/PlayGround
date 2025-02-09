bfc454162cd4ffeba9fe6c4693190f7141d31848
GZhonghui <mail@gzher.com>
2025-02-09 20:06:42 +0900

    [ACL] 补充注释

--- AtCoder/Library_Practice_Contest/A_Disjoint_Set_Union.cpp
+++ AtCoder/Library_Practice_Contest/A_Disjoint_Set_Union.cpp
@@ -2,7 +2,8 @@
 
 一些ACL共通的内容：
 区间都是左闭右开，下标都是0-indexed，C++17以上语言标准
-https://atcoder.github.io/ac-library/production/document_en/index.html
+Doc: https://atcoder.github.io/ac-library/production/document_en/index.html
+AtCoder Library Practice Contest: https://atcoder.jp/contests/practice2
 
 并查集
 
